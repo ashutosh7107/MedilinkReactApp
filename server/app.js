775dd1deb732cf3ff1c7ec49d4de144f4f9e5d16
Ashutosh Srivastava <srivastava_ashutosh@lilly.com>
2025-06-23 10:34:44 +0530

    Added my frontend url as cors origin

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: https://medilinkreactapp.onrender.com,
+    origin: "https://medilinkreactapp.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
